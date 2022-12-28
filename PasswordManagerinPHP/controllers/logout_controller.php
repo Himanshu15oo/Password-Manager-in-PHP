5608cd89ea8e4a38c8622ce7b3d1d44e4bb59cae
Himanshu15oo <96330817+Himanshu15oo@users.noreply.github.com>
2022-12-28 14:13:54 +0530

    Delete PasswordManagerinPHP directory

--- PasswordManagerinPHP/controllers/logout_controller.php
+++
@@ -1,8 +0,0 @@
-<?php  
-
-	session_start();
-	// $_SESSION["email"] = null;
-	session_destroy();
-	header("Location: login.php");
-
-?>
--- PasswordManagerinPHP/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-	include("Controllers/logout_controller.php");
-
-?>
